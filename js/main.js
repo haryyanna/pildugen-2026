10afe930bae2d441bd2cb43995b6b0b1e835e467
haryyanna <ngakanharya@gmail.com>
2026-01-02 01:32:33 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,106 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+    // Welcome modal show on page load
+    document.addEventListener('DOMContentLoaded', function () {
+        setTimeout(function () {
+            var el = document.getElementById('welcomeModal');
+            if (el && typeof bootstrap !== 'undefined') {
+                var welcomeModal = new bootstrap.Modal(el);
+                // trigger zoomIn animation on modal content
+                var content = el.querySelector('.modal-content');
+                if (content) {
+                    content.classList.remove('animated','zoomIn');
+                    // force reflow to restart animation
+                    void content.offsetWidth;
+                    content.classList.add('animated','zoomIn');
+                }
+                welcomeModal.show();
+                var enterBtn = document.getElementById('enterSiteBtn');
+                if (enterBtn) {
+                    enterBtn.addEventListener('click', function () {
+                        welcomeModal.hide();
+                    });
+                }
+            }
+        }, 800);
+    });
+
+})(jQuery);
+
